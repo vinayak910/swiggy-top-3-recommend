74f1d2e48f8f64d6906c45714a4a4526595280bb
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-03-31 16:23:58 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will suggest top 3 food items present in swiggy',
+    author='Vinayak Chhabra',
+    license='',
+)
